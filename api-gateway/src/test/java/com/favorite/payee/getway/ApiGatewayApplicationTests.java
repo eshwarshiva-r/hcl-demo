ef2515ffa2a7543c6818139ad89cc9f2ee5792ba
Praveen8722 <praveengoud8722@gmail.com>
2026-04-25 12:18:46 +0530

    first commit

---
+++ api-gateway/src/test/java/com/favorite/payee/getway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favorite.payee.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
